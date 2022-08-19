4fa959a9417d7e6f6e23814a3a0273ee873c1cd8
Mike Ditton <mike@ditton.ch>
2022-08-19 15:20:49 +0200

    choose your own input language and added more  languages for translation

--- funtranslate.sh
+++ funtranslate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-langArr=("ba" "eu" "be" "bn" "bs" "bg" "af" "sq" "am" "ar" "hy" "az" "yue" "ca" "ceb" "ny" "hr" "co" "ceb" "he" "mrj" "hi" "hmn" "hu" "is" "ig" "kn");
-echo "your text: "
+langArr=("ba" "eu" "be" "bn" "bs" "bg" "af" "sq" "am" "ar" "hy" "az" "yue" "ca" "ceb" "ny" "hr" "co" "ceb" "he" "mrj" "hi" "hmn" "hu" "is" "ig" "kn" "pt" "pa" "ro" "sd" "sn" "es" "su" "sw" "tt");
+echo "What's your input language? [en/de]"
+read inputLang
+echo "Your text: "
 read inputText
 for (( i=1; i<=30; i++ ))
 do 
@@ -10,4 +12,4 @@ done
 echo ""
 echo ""
 echo ""
-trans -brief :de "${translatedText}"
+trans -brief :$inputLang "${translatedText}"
