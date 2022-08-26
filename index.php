e192162a46a37f9727dfdbe0f6e71af48d7ff66c
Mike Ditton <mike@ditton.ch>
2022-08-26 15:02:20 +0200

    initial php version

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" src="./css/base.css">
+    <script src="./js/base.js" deffer></script>
+    <title>Document</title>
+</head>
+<body>
+    <textarea id="input-translation" placeholder="Input Text to be translated...">
+
+    </textarea>
+    <div id="output-container"><p id="output"></p></div>
+</body>
+</html>
